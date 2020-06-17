99b6fb1ad18e2ff8a73ce2768929fcb419d3b537
maxep <me@maxep.me>
2020-06-17 11:08:34 +0200

    Fix lint checks

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,34 +4,49 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import Audio, { Source, usePlaybackState } from 'react-native-audio';
 
 export default function App() {
-
-  const state = usePlaybackState()
+  const state = usePlaybackState();
 
   React.useEffect(() => {
-    console.log(state)
-  }, [state])
+    console.log(state);
+  }, [state]);
 
   // 'http://icecast.lyl.live/live'
 
   const media: Source = {
     uri: 'http://d19bhbirxx14bg.cloudfront.net/chopin-28-4-pfaul.mp3',
     title: 'Prelude in E minor Op.28 No.4',
-    artwork: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Frederic_Chopin_photo.jpeg',
+    artwork:
+      'https://upload.wikimedia.org/wikipedia/commons/e/e8/Frederic_Chopin_photo.jpeg',
     description: 'Frédéric Chopin',
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Image style={styles.artwork} source = { require('./chopin.png') }/>
+      <Image style={styles.artwork} source={require('./chopin.png')} />
       <View style={styles.controls}>
-        <TouchableOpacity style = { styles.control } onPress = { () => { Audio.play(media) } }>
-            <Icon name = { 'play' } size = { 30 }/>
+        <TouchableOpacity
+          style={styles.control}
+          onPress={() => {
+            Audio.play(media);
+          }}
+        >
+          <Icon name={'play'} size={30} />
         </TouchableOpacity>
-        <TouchableOpacity style = { styles.control } onPress = { () => { Audio.pause() } }>
-            <Icon name = { 'pause' } size = { 30 }/>
+        <TouchableOpacity
+          style={styles.control}
+          onPress={() => {
+            Audio.pause();
+          }}
+        >
+          <Icon name={'pause'} size={30} />
         </TouchableOpacity>
-        <TouchableOpacity style = { styles.control } onPress = { () => { Audio.stop() } }>
-            <Icon name = { 'stop' } size = { 30 }/>
+        <TouchableOpacity
+          style={styles.control}
+          onPress={() => {
+            Audio.stop();
+          }}
+        >
+          <Icon name={'stop'} size={30} />
         </TouchableOpacity>
       </View>
     </View>
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,50 @@
-import { useState, useEffect } from 'react'
-import { AppRegistry, NativeModules, Platform, NativeEventEmitter, DeviceEventEmitter, TaskProvider } from 'react-native'
+import { useState, useEffect } from 'react';
+import {
+  AppRegistry,
+  NativeModules,
+  Platform,
+  NativeEventEmitter,
+  DeviceEventEmitter,
+  TaskProvider,
+} from 'react-native';
 // @ts-ignore
-import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource'
+import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
 
-const { Audio } = NativeModules
-const emitter = Platform.OS === 'ios' ? new NativeEventEmitter(Audio) : DeviceEventEmitter
+const { Audio } = NativeModules;
+const emitter =
+  Platform.OS === 'ios' ? new NativeEventEmitter(Audio) : DeviceEventEmitter;
 
 // MARK: - Helpers
 
 function resolveAsset(source?: number | string) {
-  if (!source) return undefined
-  return resolveAssetSource(source)?.uri || source
+  if (!source) return undefined;
+  return resolveAssetSource(source)?.uri || source;
 }
 
 function registerAudioService(task: TaskProvider) {
   if (Platform.OS === 'android') {
-      // Registers the headless task
-      AppRegistry.registerHeadlessTask('react-native-audio', task);
+    // Registers the headless task
+    AppRegistry.registerHeadlessTask('react-native-audio', task);
   } else {
-      // Initializes and runs the service in the next tick
-      setImmediate(task());
+    // Initializes and runs the service in the next tick
+    setImmediate(task());
   }
 }
 
 // MARK: - Audio Controls
 
 const play = (source: Source): Promise<void> => {
-  source = { ...source }
+  source = { ...source };
 
-  source.uri = resolveAsset(source.uri)
-  source.artwork = resolveAsset(source.artwork)
+  source.uri = resolveAsset(source.uri);
+  source.artwork = resolveAsset(source.artwork);
 
-  return Audio.play(source)
-}
+  return Audio.play(source);
+};
 
-const pause = (): Promise<void> => Audio.pause()
-const resume = (): Promise<void> => Audio.resume()
-const stop = (): Promise<void> => Audio.stop()
+const pause = (): Promise<void> => Audio.pause();
+const resume = (): Promise<void> => Audio.resume();
+const stop = (): Promise<void> => Audio.stop();
 
 // MARK: - Data
 
@@ -48,31 +56,31 @@ export enum State {
 }
 
 export enum Event {
-  State = 'player-state',
+  PlaybackState = 'player-state',
   Progress = 'player-progress',
 }
 
 export type Source = {
-  uri: string,
-  title: string,
-  description?: string,
-  artwork?: string,
-}
+  uri: string;
+  title: string;
+  description?: string;
+  artwork?: string;
+};
 
 /**
  * @description
  *   Get playback state
  */
 export const usePlaybackState = (): State => {
-  const [state, setState] = useState(State.Unknown)
+  const [state, setState] = useState(State.Unknown);
 
   useEffect(() => {
-      const subscription = emitter.addListener(Event.State, setState)
-      return () => subscription.remove()
-  }, [])
+    const subscription = emitter.addListener(Event.PlaybackState, setState);
+    return () => subscription.remove();
+  }, []);
 
-  return state
-}
+  return state;
+};
 
 export default {
   registerAudioService,
@@ -80,4 +88,4 @@ export default {
   pause,
   resume,
   stop,
-}
+};
